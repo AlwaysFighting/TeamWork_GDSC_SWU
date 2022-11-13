01aa694ec700404fa3cf8805382ba2dcdb0a140c
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2022-11-13 20:32:05 +0900

    수정
    Tab 색상 및 아이콘 변경

--- MemoApp/MemoApp/Controller/CheckList/ChecklistViewController.swift
+++ MemoApp/MemoApp/Controller/CheckList/ChecklistViewController.swift
@@ -5,7 +5,7 @@ class ChecklistViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .darkGray
-
+        UITabBar.appearance().tintColor = .TabColor
     }
     
 }
