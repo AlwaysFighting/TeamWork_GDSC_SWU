ab002d6b1a270dbe584d7e1f00c5b3c135590a72
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2022-11-21 17:32:55 +0900

    feat : bucketlist 생성

---
+++ MemoApp/MemoApp/Data/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  MemoApp
+//
+//  Created by nayeon  on 2022/11/16.
+//
+
+import Foundation
+
+//class Note {
+//    var content: String
+//    var insertDate: Date
+//
+//    init(content: String){
+//        self.content = content
+//        insertDate = Date()
+//
+//    }
+//
+//    static var dummyNoteList = [
+//        Note(content: "일기"),
+//        Note(content: "오늘의 계획")
+//    ]
+//}
